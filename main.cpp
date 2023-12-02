0e78c5892efd86c94291bc88739045bf5feea717
Adam Goldberg <adam_goldberg25@yahoo.com>
2023-12-01 21:22:23 -0600

    measure

--- main.cpp
+++ main.cpp
@@ -8,11 +8,16 @@
 #include <stdio.h>
 #include <iostream>
 #include <string>
+#include <chrono>
 
 #include "include/trebuchet.hpp"
 
 int main()
 {
+
+    auto const start = std::chrono::steady_clock::now();
     auto const count = aoc::trebuchet::GetCountWords("input/trebuchet.txt");
+    auto const end = std::chrono::steady_clock::now();
     std::cout << "Count: " << std::to_string(count) << std::endl;
+    std::cout << "Time: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << std::endl;
 }
