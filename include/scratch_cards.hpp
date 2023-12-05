7ecf546734493601459253809eac6fa34ab99877
Adam Goldberg <adam_goldberg25@yahoo.com>
2023-12-04 20:30:19 -0600

    close, but no cigar

--- include/scratch_cards.hpp
+++ include/scratch_cards.hpp
@@ -13,14 +13,47 @@ namespace aoc
 {
   class scratch_cards
   {
+    struct ScratchCard
+    {
+        std::size_t numMatching{};
+        int numWon{};
+    };
     public:
         static int GetScratchSum(std::string const& filePath)
+        {
+            auto scratches = GetScratchCards(filePath);
+            int count = 0;
+            std::cout << "total lines: " << scratches.size() << std::endl;
+            for(auto i = 0; i < scratches.size(); i++)
+            {
+                auto const& scratch = scratches.at(i);
+                count += scratch.numWon;
+
+                auto const next = i + 1;
+                auto const end = next + scratch.numMatching;
+                std::cout << "next: " << next << " end: " << end << std::endl;
+                for(auto j = next; j < end; j++)
+                {
+                    auto val = 1;
+                    if(scratch.numWon != 0)
+                    {
+                        val = scratch.numWon;
+                    }
+                    scratches.at(j).numWon += val;
+                }
+                std::cout << "num matching: " << scratch.numMatching << " | " << "num won: " << scratch.numWon << std::endl;
+            }
+            return count + scratches.size();
+        }
+
+    private:
+        static std::vector<ScratchCard> GetScratchCards(std::string const& filePath)
         {
             std::fstream f;
             f.open(filePath);
             std::string line;
 
-            auto count = 0;
+            std::vector<ScratchCard> scratches{};
             while(getline(f, line))
             {
                 // split on "|", left set is winners, right set is our num
@@ -30,13 +63,11 @@ namespace aoc
                 std::vector<std::string> intersect{};
                 std::set_intersection(winning.begin(), winning.end(), myNums.begin(), myNums.end(), std::back_inserter(intersect));
                 auto const numOverlapping = intersect.size();;
-                if(numOverlapping == 0)
-                {
-                    continue;
-                }
-                count +=  (1 << (numOverlapping - 1));
+                scratches.emplace_back(ScratchCard{
+                    .numMatching = numOverlapping
+                });
             }
-            return count;
+            return scratches;
         }
   };
 }
